2b02184e81f8b8ad2e4f8d2d1bc75f315c2f732c
admiraldy <admiraldyfz@gmail.com>
2023-08-18 20:56:47 +0700

    Create nomor2.dart

---
+++ Week 1/03_fundamental-dart/praktikum/soal_prioritas_2/nomor2.dart
@@ -0,0 +1,13 @@
+void main(){
+
+// define pi, jari-jari, dan tinggi tabung
+  double pi = 3.14;
+  double r = 10;
+  double t = 30;
+
+// rumus volume tabung
+  double volumeTabung = pi * r * r * t;
+
+// show output
+  print("volume tabung adalah $volumeTabung");
+}
