1eac5c9f569d04ea9195f20b6a80186c5deaa7d7
admiraldy <admiraldyfz@gmail.com>
2023-08-18 20:56:44 +0700

    Create nomor1.dart

---
+++ Week 1/03_fundamental-dart/praktikum/soal_prioritas_2/nomor1.dart
@@ -0,0 +1,10 @@
+void main(){
+
+// define 3 variabel tipe data string
+  String nama = "Admiraldy Fahruz Zaman";
+  String programMSIB = "Alterra 2023 Complete Front-End Engineer Career with Flutter";
+  String kelas = "Kelas A";
+
+// show output dari ketiga variabel tipe data string
+  print("$nama, $programMSIB, $kelas");
+}
