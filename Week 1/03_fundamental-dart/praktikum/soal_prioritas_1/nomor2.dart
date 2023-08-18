d278ed54b30a077502b2450e62b8c132523e8b55
admiraldy <admiraldyfz@gmail.com>
2023-08-18 20:56:31 +0700

    Create nomor2.dart

---
+++ Week 1/03_fundamental-dart/praktikum/soal_prioritas_1/nomor2.dart
@@ -0,0 +1,13 @@
+void main(){
+
+// define pi dan jari-jari lingkaran
+  double r = 28;
+  double pi = 22/7;
+
+// rumus keliling dan luas lingkaran
+  double kelilingLingkaran = 2 * pi * r;
+  double luasLingkaran = pi * (r * r);
+
+// show output
+  print("keliling dan luas lingkaran adalah $kelilingLingkaran & $luasLingkaran");
+}
